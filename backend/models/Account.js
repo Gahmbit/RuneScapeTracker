f26c8a5fcc0e339b5dffe1f754424f38e7aa914d
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-15 23:17:59 -0500

    build: added Account model

---
+++ backend/models/Account.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AccountSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  addedBy: String,
+  snapshots: Array,
+});
+
+module.exports = mongoose.model("Account", AccountSchema);
