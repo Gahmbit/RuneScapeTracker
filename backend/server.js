abb68af968d36861c0a6a3ed0cdbbaeb07b8f123
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-17 23:28:06 -0500

    testing any username query > good

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,29 @@ connectDB();
 
 app.get("/", (req, res) => {
   res.send("working!");
+  console.log("pinged at /");
 });
+//Below code is just to test api calling > it works lol
+async function findUser(account) {
+  const userData = await fetch(
+    `https://apps.runescape.com/runemetrics/profile/profile?user=${account}&activities=20`
+  );
+  const userJSON = await userData.json();
+  return userJSON;
+}
 
+app.get("/:id", (req, res) => {
+  findUser(`${req.params.id}`)
+    .then((userData) => {
+      res.send(userData);
+      console.log(`Showing data for "${req.params.id}", from ${req.ip}`);
+    })
+    .catch((err) => {
+      res.send(err);
+      console.log(err);
+    });
+});
+// testing ends here
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
