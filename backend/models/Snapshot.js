fb4d040bb6dfcd250f5dbbdd24571b086485a8a3
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-17 21:15:48 -0500

    added Snapshot schema, added template of Runescape API object for reference

---
+++ backend/models/Snapshot.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+//all below data acquired via https://apps.runescape.com/runemetrics/profile/profile?user=X&activities=20, unless stated otherwise
+const SnapshotSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  rank: String,
+  totalSkill: Number,
+  totalExp: Number,
+  combatLevel: Number,
+  activities: Array,
+  questPoints: Number, //acquired via https://apps.runescape.com/runemetrics/quests?user=X
+  skills: {
+    type: Map,
+    of: new Schema({
+      level: Number,
+      experience: Number,
+      rank: Number,
+    }),
+  },
+});
