2afb66b4cdd2e3875e9b8c0225dfbad23a59e448
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-18 19:27:36 -0500

    fixed dotenv in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const accountRouter = require("./routes/Account");
+require("dotenv").config({ path: __dirname + "./vars/.env" });
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("working!");
