c8287be8b466fed85a04af36ab97cc03a2bd52ef
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-21 20:53:50 -0500

    added CORS to server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,15 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("working!");
   console.log(`GET request @ /, from ${req.ip}`);
