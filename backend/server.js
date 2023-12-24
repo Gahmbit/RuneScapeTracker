0e57ca3a7497063760684b58e9af2a8fe24d65c6
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-23 23:42:34 -0500

    added ratelimiter to server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const accountRouter = require("./routes/Account");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
+//Rate Limiter Settings (Currently 100 requests per 10 minutes)
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+});
+
+//Middleware
 app.use(cors());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.send("working!");
-  console.log(`GET request @ /, from ${req.ip}`);
 });
 
 //routers
