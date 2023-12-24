f31bbeea7f285eee7c16d86cfe1e9c80193bff39
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-23 23:38:29 -0500

    added official cors package and configured

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const accountRouter = require("./routes/Account");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("working!");
