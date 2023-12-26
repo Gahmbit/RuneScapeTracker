63530135245081e53ffae9de4d1470ffa6ce2e8d
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-26 15:00:56 -0500

    added footer

---
+++ frontend/src/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>Gahmbit, 2023.</h3>
+      <h4>
+        RuneScape is the trademark of{" "}
+        <a href="http://jagex.com/" target="_blank">
+          Jagex Limited.
+        </a>
+      </h4>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -1,22 +1,26 @@
 // import React from "react";
+import Footer from "./Footer";
 import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
-    <div className="landing">
-      <div className="header">
-        <h1>
-          <span id="yellow">RuneScape</span> Tracker
-        </h1>
+    <>
+      <div className="landing">
+        <div className="header">
+          <h1>
+            <span id="yellow">RuneScape</span> Tracker
+          </h1>
+        </div>
+        <div className="searching">
+          <form>
+            <input name="rsn" placeholder="Enter your RuneScape Username" />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+        <h2>*Only works for RuneScape 3 - sorry nostalgists!</h2>
       </div>
-      <div className="searching">
-        <form>
-          <input name="rsn" placeholder="Enter your RuneScape Username" />
-          <button type="submit">Search</button>
-        </form>
-      </div>
-      <h2>*Only works for RuneScape 3 - sorry nostalgists!</h2>
-    </div>
+      <Footer />
+    </>
   );
 };
 
