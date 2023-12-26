2446463e3257a00f9090115b3ca7a9adc77aa673
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-26 15:07:24 -0500

    added links to footer

--- frontend/src/Footer.tsx
+++ frontend/src/Footer.tsx
@@ -3,7 +3,16 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h3>Gahmbit, 2023.</h3>
+      <h4>
+        <a href="https://github.com/Gahmbit/" target="_blank">
+          Gahmbit
+        </a>{" "}
+        &{" "}
+        <a href="https://github.com/the-code-raccoon" target="_blank">
+          The Code Raccoon
+        </a>
+        , 2023.
+      </h4>
       <h4>
         RuneScape is the trademark of{" "}
         <a href="http://jagex.com/" target="_blank">
