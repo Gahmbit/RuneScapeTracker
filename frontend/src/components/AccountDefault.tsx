9bbe1fb3ef22b3db092d62864b3ba630370f4bca
Gahmbit <14244387+Gahmbit@users.noreply.github.com>
2023-12-27 15:21:05 -0500

    page currently spits out current user data as JSON

--- frontend/src/components/AccountDefault.tsx
+++ frontend/src/components/AccountDefault.tsx
@@ -3,11 +3,13 @@ import "../styles/AccountDefault.css";
 
 const AccountDefault = ({ rsn }: { rsn: string | undefined }) => {
 
-  const [userData, setUserData] = useState("Loading...");
+  const [userData, setUserData] = useState(null);
+  const [isLoading, setLoading] = useState<boolean>(true);
 
   async function getUserData(){
     const response = await fetch(`https://runescape-tracker-api.onrender.com/account/${rsn}`);
     const rsData = await response?.json();
+    setLoading(false);
     setUserData(rsData);
   }
 
@@ -17,8 +19,8 @@ const AccountDefault = ({ rsn }: { rsn: string | undefined }) => {
 
   return (
     <div className="accountDefault">
-      <h1>{rsn}'s Stats</h1>
-      <p>{JSON.stringify(userData)}</p>
+      {isLoading && <h1>Loading {rsn}'s Stats...</h1>}
+      {userData && <p>{JSON.stringify(userData)}</p>}
     </div>
   );
 };
