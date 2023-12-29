1af51f66a22b813cf75bd987d7a1199586b2068c
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-28 23:59:36 -0500

    css layout for adventure log 90% there

--- frontend/src/components/AccountAdventurersLog.tsx
+++ frontend/src/components/AccountAdventurersLog.tsx
@@ -7,34 +7,7 @@ type Props = {
     accountData: Account;
 };
 
-// enum LogType {
-//     Skill = "skill",
-//     Quest = "quest",
-//     Kill = "kill",
-//     Drop = "drop",
-//     Default = "default",
-// }
-
 const AccountAdventurersLog = ({ accountData }: Props) => {
-    // const skills = Object.keys(accountData.skills).map((key) => {
-    //     return {
-    //         skillName: key,
-    //         maxSkillLevel: [
-    //             "Dungeoneering",
-    //             "Invention",
-    //             "Slayer",
-    //             "Farming",
-    //             "Herblore",
-    //             "Archaeology",
-    //             "Necromancy",
-    //         ].includes(key)
-    //             ? 120
-    //             : 99,
-    //         ...accountData.skills[key as Skills],
-    //     };
-    // });
-    // .sort(sortFunctions[selectSort]);
-
     const activities = accountData.activities;
     console.log(activities);
 
@@ -42,19 +15,22 @@ const AccountAdventurersLog = ({ accountData }: Props) => {
         <div className="account-activities">
             <div className="account-activities__title">Activity Log</div>
             <div className="account-activities__list">
-                {activities.map((skill) => {
+                {activities.map((activity) => {
                     return (
                         <div className="account-activities__stat">
                             <img
-                                src={icons[skill.type + "Icon"]}
+                                src={icons[activity.type + "Icon"]}
                                 className="account-activities__stat__icon"
                             />
+                            <div className="account-activities__stat__date">
+                                {activity.date.split(" ")[0]}
+                            </div>
                             <div className="account-activities__stat__info">
                                 <div className="account-activities__stat__info__name">
-                                    {skill.text}
+                                    {activity.text}
                                 </div>
-                                <div className="account-activities__stat__info__rank-xp">
-                                    <div>{skill.details}</div>
+                                <div className="account-activities__stat__info__details">
+                                    <div>{activity.details}</div>
                                 </div>
                             </div>
                         </div>
