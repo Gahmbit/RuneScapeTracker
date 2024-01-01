5156cf7da5ea86eb60c030f2509f56f5cc970fa2
Gambhit <14244387+Gahmbit@users.noreply.github.com>
2023-12-31 19:52:07 -0500

    changed version to 0.8.0

--- frontend/src/components/Version.tsx
+++ frontend/src/components/Version.tsx
@@ -3,7 +3,7 @@ import "../styles/Version.css";
 const Version = () => {
     return (
         <div className="version">
-            <h3>Version 1.0.0</h3>
+            <h3>Version 0.8.0</h3>
         </div>
     );
 };
