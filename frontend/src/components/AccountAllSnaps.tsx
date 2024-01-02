30fc18b52da5ed6926a227401f4cb1bc9160bda4
Gahmbit <14244387+Gahmbit@users.noreply.github.com>
2024-01-02 15:28:47 -0500

    added buttons, need to route

--- frontend/src/components/AccountAllSnaps.tsx
+++ frontend/src/components/AccountAllSnaps.tsx
@@ -1,12 +1,19 @@
 import "../styles/AccountAllSnaps.css";
 import icons from "../assets";
 import { Account } from "../types/Account.tsx";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     allSaves: Array<Account> | undefined;
 };
 
 const AccountStats = ({ allSaves }: Props) => {
+    const navigate = useNavigate();
+
+    const goTo = (id: string) => {
+        navigate(id);
+    };
+
     return (
         <div className="account-stats">
             <div className="account-stats__title">Saved Stats</div>
@@ -16,26 +23,36 @@ const AccountStats = ({ allSaves }: Props) => {
                 {allSaves?.map((save) => {
                     return (
                         <div className="save-snap" key={save?._id}>
-                            <div className="save-snap_details">
-                                <img
-                                    src={icons["skillIcon"]}
-                                    alt="Total Skill"
-                                />
-                                <h2>{save.totalSkill}</h2>
-                                <img
-                                    src={icons["killIcon"]}
-                                    alt="Combat Level"
-                                />
-                                <h2>{save.combatLevel}</h2>
-                                <img src={icons["highScoreIcon"]} alt="Rank" />
-                                <h2>{save.rank.toLocaleString()}</h2>
-                                <img
-                                    src={icons["achievementIcon"]}
-                                    alt="Total XP"
-                                />
-                                <h2>{save.totalExp.toLocaleString()}</h2>
-                            </div>
-                            <h1>{new Date(save?.timestamp).toUTCString()}</h1>
+                            <button
+                                className="save-snap_button"
+                                // onClick={goTo(save?._id)}
+                            >
+                                <div className="save-snap_details">
+                                    <img
+                                        src={icons["skillIcon"]}
+                                        alt="Total Skill"
+                                    />
+                                    <h2>{save.totalSkill}</h2>
+                                    <img
+                                        src={icons["killIcon"]}
+                                        alt="Combat Level"
+                                    />
+                                    <h2>{save.combatLevel}</h2>
+                                    <img
+                                        src={icons["highScoreIcon"]}
+                                        alt="Rank"
+                                    />
+                                    <h2>{save.rank.toLocaleString()}</h2>
+                                    <img
+                                        src={icons["achievementIcon"]}
+                                        alt="Total XP"
+                                    />
+                                    <h2>{save.totalExp.toLocaleString()}</h2>
+                                </div>
+                                <h1>
+                                    {new Date(save?.timestamp).toUTCString()}
+                                </h1>
+                            </button>
                         </div>
                     );
                 })}
